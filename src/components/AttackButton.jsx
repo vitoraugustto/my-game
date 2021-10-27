bf230d72c674b0f651bdb94590ea9cdedc5fc770
Vitor Augusto <vitor.augusto@assinasaude.com.br>
2021-10-27 13:48:16 -0300

    added combat

--- src/components/AttackButton.jsx
+++ src/components/AttackButton.jsx
@@ -1,51 +1,19 @@
-import { useSelectedCharacter } from "../contexts/SelectedCharacter";
 import { useEnemyHitPoints } from "../contexts/EnemyHitPoints";
-
-import { enemies } from "../characters/enemies";
-
-import "./AttackButton.css";
-import { roles } from "../characters/roles";
+import { useSelectedCharacter } from "../contexts/SelectedCharacter";
 import { persistEnemy } from "../utils/functions";
 
+import "./AttackButton.css";
+
 export const AttackButton = ({ children, attack, ...props }) => {
-  const { selectedEnemy, selectedRole } = useSelectedCharacter();
   const { enemyHitPoints, setEnemyHitPoints } = useEnemyHitPoints();
+  const { selectedEnemy } = useSelectedCharacter();
 
-  function getIntervalDamage(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  persistEnemy(selectedEnemy, enemyHitPoints);
 
-  function attack() {
-    const rawDamage = roles[selectedRole].baseStatus.baseAttack;
-    const minHitDamage = Math.round(rawDamage * 0.8);
-    const maxHitDamage = Math.round(rawDamage * 1.2);
-    const intervalDamage = getIntervalDamage(minHitDamage, maxHitDamage);
-    const criticalHitChance = parseInt(Math.random() * 100 + 1);
-    const baseCriticalHitChance = 1;
-    const weaponCriticalHitChance = 89;
-    const criticalDamage = 3;
-    const multiplier = baseCriticalHitChance + weaponCriticalHitChance;
-    const rawFinalDamage =
-      criticalHitChance <= multiplier
-        ? intervalDamage * criticalDamage
-        : intervalDamage;
-
-    let finalDamage =
-      rawFinalDamage - enemies[selectedEnemy].baseStatus.baseDefense;
-
-    if (finalDamage <= 0) finalDamage = 0;
-
-    persistEnemy(selectedEnemy, enemyHitPoints);
-    return finalDamage;
-  }
   return (
     <button
       style={{ margin: props.margin }}
-      onClick={() =>
-        setEnemyHitPoints(
-          selectedEnemy && selectedRole !== "" ? enemyHitPoints - attack() : ""
-        )
-      }
+      onClick={() => setEnemyHitPoints(enemyHitPoints - attack())}
       className="attack-button"
     >
       {children}
---
+++ src/components/Combat.jsx
@@ -0,0 +1,59 @@
+import { AttackButton } from "./AttackButton";
+import { PlayerCoins } from "./PlayerCoins";
+import { Profile } from "./Profile";
+
+import { useSelectedCharacter } from "../contexts/SelectedCharacter";
+import { useEnemyHitPoints } from "../contexts/EnemyHitPoints";
+
+import { getIntervalDamage, persistEnemy } from "../utils/functions";
+import { roles } from "../characters/roles";
+import { enemies } from "../characters/enemies";
+import { useState } from "react";
+
+export const Combat = () => {
+  const { selectedEnemy, selectedRole } = useSelectedCharacter();
+
+  const [critical, setCritical] = useState();
+
+  function attack() {
+    const rawDamage = roles[selectedRole].baseStatus.baseAttack;
+    const minHitDamage = Math.round(rawDamage * 0.8);
+    const maxHitDamage = Math.round(rawDamage * 1.2);
+    const intervalDamage = getIntervalDamage(minHitDamage, maxHitDamage);
+    const criticalHitChance = parseInt(Math.random() * 100 + 1);
+    const baseCriticalHitChance = 1;
+    const weaponCriticalHitChance = 49;
+    const criticalDamage = 3;
+    const multiplier = baseCriticalHitChance + weaponCriticalHitChance;
+    const rawFinalDamage =
+      criticalHitChance <= multiplier
+        ? intervalDamage * criticalDamage
+        : intervalDamage;
+
+    if (criticalHitChance <= multiplier) {
+      setCritical(true);
+    } else {
+      setCritical(false);
+    }
+
+    let finalDamage =
+      rawFinalDamage - enemies[selectedEnemy].baseStatus.baseDefense;
+
+    if (finalDamage <= 0) finalDamage = 0;
+
+    return finalDamage;
+  }
+
+  return (
+    <div style={{ margin: "0 auto", maxWidth: "400px" }}>
+      <PlayerCoins marginBottom={"10px"} />
+      <Profile isEnemy={false} />
+
+      <AttackButton attack={attack} margin={"10px 0"}>
+        Atacar
+      </AttackButton>
+
+      <Profile isEnemy={true} />
+    </div>
+  );
+};
